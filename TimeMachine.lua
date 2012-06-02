edcbe24eabbca8d68e35ca04f7555702b7e077e3
Thomas Gorence <thomas@producerism.com>
2012-06-02 07:29:02 -0400

    added some improvements via Jared at PagodaWest Games (thanks!)

--- TimeMachine.lua
+++ TimeMachine.lua
@@ -5,46 +5,51 @@ M.fps = 30
 local mFloor= math.floor
 local prevTime, frameCounter = 0, 0
 local baseTimeStep, curTimeStep = M.fps, M.fps
- 
+local bTimeMachineOn = false
 M.updateInterval = 1
  
-physics.setTimeStep(1/baseTimeStep)
+physics.setTimeStep (0)
  
 M.adjustFPS = function()
-  local curTime = system.getTimer()
-  local dt = curTime - prevTime
-  prevTime = curTime
-  local fps = mFloor(1000/dt)
- 
-  -- CHECK EVERY N FRAMES
-  if frameCounter == M.updateInterval then
- 
- 
-     if fps < baseTimeStep then
-        -- DROP IN FPS, SET NEW TIME STEP
-        physics.setTimeStep(1/fps)
-        curTimeStep = fps
-     else
-        -- REVERT BACK TO BASE TIME STEP
-        if curTimeStep < baseTimeStep then
-             physics.setTimeStep(1/baseTimeStep)
-             curTimeStep = baseTimeStep
-        end
-     end
- 
-    frameCounter = 0
-    --print(fps)
-  end
- 
-  frameCounter = frameCounter + 1
+	if bTimeMachineOn then
+		local curTime = system.getTimer()
+		local dt = curTime - prevTime
+		prevTime = curTime
+		local fps = mFloor (1000/dt)
+		local timeStep
+		
+		-- CHECK EVERY N FRAMES
+		if (frameCounter == M.updateInterval) then 
+			if (fps < baseTimeStep) then
+				-- DROP IN FPS, SET NEW TIME STEP
+				physics.setTimeStep (1/fps)
+				curTimeStep = fps
+				timeStep = fps
+			else
+				-- REVERT BACK TO BASE TIME STEP
+				physics.setTimeStep (0)
+				curTimeStep = baseTimeStep
+				timeStep = baseTimeStep
+			end
+		 
+			frameCounter = 0
+			--print (timeStep)
+		end
+	
+		frameCounter = frameCounter + 1
+	end
 end
  
 M.start = function()
-  Runtime:addEventListener("enterFrame", M.adjustFPS)
+	prevTime, frameCounter = 0, 0
+	physics.setTimeStep (0)
+	bTimeMachineOn = true
+	Runtime:addEventListener("enterFrame", M.adjustFPS)
 end
  
 M.stop = function()
-  Runtime:removeEventListener("enterFrame", M.adjustFPS)
+	bTimeMachineOn = false
+	Runtime:removeEventListener("enterFrame", M.adjustFPS)
 end
  
 return M
